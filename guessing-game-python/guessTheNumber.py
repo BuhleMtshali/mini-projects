61712ed12194d8ae23dc96b6cb33de5c07241c89
Buhle <mtshalizanobuhle7@gmail.com>
2026-01-08 21:15:40 +0200

    updated the gueess the number

--- guessing-game-python/guessTheNumber.py
+++ guessing-game-python/guessTheNumber.py
@@ -22,12 +22,23 @@ def guess_the_number():
 
         #STARTING THE INNER LOOP TO RETRY IF OUT OF TRIES
         while True:
+
             #FIRST EVALUATE IF THE USER INPUTED A NUMBER
             try:
                 guess = int(user_guess)
             except ValueError:
                 print("🚫 Invalid Input, please ensure ypu input a number")
 
+            if guess == random_number:
+                print(f"HAZZAH🎰 You guessed {guess} and the correct number was {random_number} 👾 You Win🎊")
+                break
+            elif guess > random_number:
+                attempts -= 1
+                print(f"Ooops 🐝 Too {guess} is too High, You have {attempts} attempts left ‼️")
+            elif guess < random_number:
+                attempts -= 1
+                print(f"Ooops 🐝 Too {guess} is too Low, You have {attempts} attempts left ‼️")
+            
 
 
         #CLOSING THE MAIN LOOP
