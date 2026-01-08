39c3276ffc52988ff8350b53c78250ba325b963f
Buhle <mtshalizanobuhle7@gmail.com>
2026-01-08 20:44:45 +0200

    created the random number

--- guessing-game-python/guessTheNumber.py
+++ guessing-game-python/guessTheNumber.py
@@ -1,5 +1,11 @@
-#IMPORTING THREADING
+#IMPORTING THREADING & RANDOM
 import threading
+import random
 
 #WELCOME MESSAGE
 print("===== 🎲 Mini Number Guessing Game 🎮 ====")
+
+#MAIN FUNCTION
+
+def guess_the_number():
+    random_number = random.randint(1, 20)
