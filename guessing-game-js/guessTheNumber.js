3422f6a95036a5df99e87ef4a3dd927b2f05ff6d
Buhle <mtshalizanobuhle7@gmail.com>
2026-01-08 19:12:25 +0200

    updated the gueess the number loop

--- guessing-game-js/guessTheNumber.js
+++ guessing-game-js/guessTheNumber.js
@@ -9,11 +9,37 @@ setTimeout(() => {
 
 // MAIN FUNCTION
 function guessTheNumber(){
+
     //STARTING THE LOOP FOR THE MAIN GAME
     let playAgain = "yes";
 
     while(playAgain.toLowerCase() === "yes"){
-        console.log("loop running")
+        let randomNumber = Math.round(Math.random() * 20) + 1;
+        let attempts = 5 //THIS COUNTS THE ATTEMPTS THE USER HAS
+
+        //CREATING THE INNER LOOP
+        while(attempts > 0){
+            let guessNumber = parseInt(prompt("🎮 Choose A random number (1-20): "))
+
+            //CHECKING IF NUMBER MATCHES
+            if(isNaN(guessNumber)){
+                console.log("🚫 Invalid Number old Chap ‼️");
+            } else if(guessNumber === randomNumber){
+                console.log(`HAZZAH!! 🐝 You guessed ${guessNumber} and the correct number was actually ${randomNumber}`);
+                break;
+            } else if(guessNumber > randomNumber){
+                attempts--;
+                console.log(`Ouch ‼️ ${guessNumber} is too High, try Again, you have ${attempts} attempts left`);
+            } else if(guessNumber < randomNumber){
+                attempts--;
+                console.log(`Ouch ‼️ ${guessNumber} is too low, try Again, you have ${attempts} attempts left`);
+            }
+
+            //THIS IS FOR IF THE ATTEMPTS 0
+            if(attempts === 0){
+                console.log(`🎲 Oooh Ooh you have ${attempts} attempts left, Try replaying the game 🎰`);
+            }
+        }
 
 
         //ASKING IF THEY WANNA PLAY AGAIN
