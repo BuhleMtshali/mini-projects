0c7a68d142b5e9af664e3ba1a2c70daf6c32297d
Buhle <mtshalizanobuhle7@gmail.com>
2026-01-08 22:11:34 +0200

    updated the while loop

--- java-guessing-game/guessTheNumber/src/guessTheNumber.java
+++ java-guessing-game/guessTheNumber/src/guessTheNumber.java
@@ -5,6 +5,16 @@ public class guessTheNumber {
         Scanner input = new Scanner(System.in);
         String playAgain = "yes";
 
+        //STARTING THE MAIN WHILE LOOP
+        while (playAgain.equalsIgnoreCase("yes")) {
+            System.out.print("loop ...");
+
+
+           //CLOSING THE MAIN LOOP
+            System.out.print("Would You like to play Another Round 🎮(yes/no): ");
+            playAgain = input.nextLine();
+        }
+
 
         input.close();
     }
