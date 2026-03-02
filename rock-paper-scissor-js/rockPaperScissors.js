39eeb16620a134d4a5d38eda7259fa8fa5ee8a37
Buhle <mtshalizanobuhle7@gmail.com>
2026-03-02 20:05:47 +0200

    updated the variable

---
+++ rock-paper-scissor-js/rockPaperScissors.js
@@ -0,0 +1,9 @@
+const prompt = require("prompt-sync")();
+
+function playTheGame(){
+    console.log("")
+    //VARIABLES
+    let playerWins = 0
+    let computerWins = 0
+    let choices = ["rock", "paper", "scissors"]
+}
