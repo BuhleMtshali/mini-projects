3131fb087ce06690ec78e36fe150e8b60737279e
Buhle <mtshalizanobuhle7@gmail.com>
2026-03-02 20:13:11 +0200

    updated the game logic

--- rock-paper-scissor-js/rockPaperScissors.js
+++ rock-paper-scissor-js/rockPaperScissors.js
@@ -1,9 +1,26 @@
 const prompt = require("prompt-sync")();
 
 function playTheGame(){
-    console.log("")
+    console.log("==== 🎲 WELCOME TO MY MINI GAME 📡 ====")
+
     //VARIABLES
     let playerWins = 0
     let computerWins = 0
     let choices = ["rock", "paper", "scissors"]
+
+    //START THE WHILE LOOP
+    let playAgain = "yes"
+    while(playAgain.toLowerCase() === "yes"){
+        console.log("loop is rtunning")
+
+
+
+        //CLOSING THE WHILE LOOP
+        playAgain = prompt("Wanna go again(yes/no): ").toLowerCase()
+    }
+
+    //CLOSING MESSAGE
+    console.log("===== ✨ THANK YOU, GOODBYE ✨ =====")
 }
+
+playTheGame()
