d80915a95e553792f92ee3fd57f6d8bcb45c6bdc
Buhle <mtshalizanobuhle7@gmail.com>
2026-04-09 15:31:25 +0200

    updated the index and stylesheet

---
+++ passanger-counter-js/app.js
@@ -0,0 +1,11 @@
+let progress = 0;
+const bar = document.getElementById('progress-bar')
+const btn = document.getElementById('increment-btn')
+
+btn.addEventListener('click', () => {
+    if(progress < 100){
+        progress += 10;
+        if(progress > 100) progress = 100
+        bar.style.width = progress + '%'
+    }
+})
