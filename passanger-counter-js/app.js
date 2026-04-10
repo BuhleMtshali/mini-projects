82c52942de76f502a5f0a398ad817c5b8a5d84b8
Buhle <mtshalizanobuhle7@gmail.com>
2026-04-10 11:15:38 +0200

    updated the js variables

---
+++ passanger-counter-js/app.js
@@ -0,0 +1,7 @@
+//DEFINING THE VARIABLES
+let passengerCount = document.querySelector("#count");
+let percentageCount = document.querySelector(".percentage");
+let progressBar = document.querySelector("#progress-bar");
+let offBoardingBtn = document.querySelector(".offboard");
+let onBoardBtn = document.querySelector(".board");
+let resetBtn = document.querySelector("#reset-btn")
