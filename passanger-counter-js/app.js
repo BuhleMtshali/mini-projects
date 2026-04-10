142d8d902533c60094ae152102b65cd07c0973cf
Buhle <mtshalizanobuhle7@gmail.com>
2026-04-10 11:21:15 +0200

    updated the js fle

--- passanger-counter-js/app.js
+++ passanger-counter-js/app.js
@@ -4,4 +4,16 @@ let percentageCount = document.querySelector(".percentage");
 let progressBar = document.querySelector("#progress-bar");
 let offBoardingBtn = document.querySelector(".offboard");
 let onBoardBtn = document.querySelector(".board");
-let resetBtn = document.querySelector("#reset-btn")
+let resetBtn = document.querySelector("#reset-btn");
+let availableSeats = document.querySelector("#available");
+let occupiedSeats = document.querySelector("#occupancy")
+let progress = 0;
+
+//1. Making the progress bar work first
+onBoardBtn.addEventListener("click", () => {
+    if(progress < 100){
+        progress += 1;
+        if(progress > 100) progress = 100
+        print.style.width = progress
+    }
+})
